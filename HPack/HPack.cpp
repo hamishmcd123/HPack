bef5f7e285fc64c3acd4f8825cc6f91b481af5b2
Hamish McDonald <s242170@students.aie.edu.au>
2026-05-12 14:04:37 +1000

    Initial commit

---
+++ HPack/HPack.cpp
@@ -0,0 +1,20 @@
+﻿// You can delete the vendored stb header files. 
+
+#ifndef STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#include "Vendor/stb_image.h"
+#else
+#include "Vendor/stb_image.h"
+#endif
+
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "Vendor/stb_image_write.h"
+#else
+#include "Vendor/stb_image_write.h"
+#endif
+
+namespace HPack {
+
+
+} //namespace HPack
---
+++ HPack/HPack.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+namespace hpack {
+
+#include <fstream>
+#include <unordered_map> 
+
+#ifndef HPACK_ASSERT
+#include <cassert>
+#define HPACK_ASSERT(_EXPR) assert(_EXPR);
+#endif
+#define HPACK_UNUSED(x) ((void)(x));
+
+	// Forward declarations
+	struct Context;
+	struct Rectangle;
+
+
+
+} //namespace hpack
+
+
